3f5018a4199ad6ccc95388289f5b8048a5e51077
User <nuridinovk24@gmail.com>
2022-11-07 13:23:57 +0600

    Kairat

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MyClass myClass = new MyClass(" Nuridinov","Kairat",18,new String[]{"Sofkils ","Englsh","java"});
+        System.out.println("Surname : "+myClass.getSurname()+"\n"+"Name : " +myClass.getName()+"\n"+"Age : "+myClass.getAge()+"\n"+"Which classes will be attended : "+ Arrays.toString(myClass.getPeaksoft()));
+
+        MyClass myClass1=new MyClass("Manty");
+        System.out.println("Food : "+myClass1.getfood());
     }
+
 }
--- src/MyClass.java
+++ src/MyClass.java
@@ -1,2 +1,68 @@
-package PACKAGE_NAME;public class MyClass {
+public class MyClass {
+    private String surname;
+    private String name;
+    private int age;
+    private String[] Peaksoft;
+    private String food;
+
+    public MyClass() {
+
+    }
+
+    public MyClass(String surname, String name, int age, String [] peaksoft ) {
+        this.surname = surname;
+        this.name = name;
+        this.age = age;
+        this.Peaksoft=peaksoft;
+
+    } public MyClass(String food) {
+        this.food = food;
+
+    }
+
+
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String[] getPeaksoft() {
+        return Peaksoft;
+    }
+
+    public void setPeaksoft(String[] peaksoft) {
+        Peaksoft = peaksoft;
+    }
+
+    public String getfood() {
+        return food;
+    }
+
+    public void setfood(String food) {
+        this.food = food;
+    }
+
+    public MyClass(String[] peaksoft) {
+        Peaksoft = peaksoft;
+    }
 }
