88c62b92362c01125e6b836527b9fd347afd4c74
User <nuridinovk24@gmail.com>
2022-11-10 16:07:21 +0600

    kairat2

---
+++ src/Circle.java
@@ -0,0 +1,12 @@
+public class Circle {
+    private static double Pi=Math.PI;
+
+    public static double area( double radius){
+
+        return Pi*(radius*radius);
+
+    }
+    public static double circumference( double radius ){
+        return Pi*2*radius;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,17 @@
-import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println();
-        Programmer programmer= new Programmer("Kairat",18,"Programist","writes code");
-        System.out.println(programmer+"\n");
 
-        Dancer dance=new Dancer("Akylai",15," Dancer","dancing");
-        System.out.println(dance+"\n");
+        Scanner sc =new Scanner(System.in);
 
-        Singer singer =new Singer("Argen",18,"Singe","sings");
-        System.out.println(singer);
+        System.out.println(" Radius санын бериниз  ");
+
+        double radius= sc.nextDouble();
+
+        System.out.println("Area : "+Circle.area(radius));
+
+        System.out.println("Circumference : "+Circle.circumference(radius));
     }
 }
